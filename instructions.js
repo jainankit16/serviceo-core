3cd7ac249a493490af0475551f0c0f50cee33873
ankitjain <ankitjain@ironsystems.com>
2019-12-19 04:26:49 -0800

    first commit

--- instructions.js
+++ instructions.js
@@ -4,11 +4,11 @@ const path = require('path')
 
 async function createConfigFile(cli) {
     try {
-        await cli.copy(path.join(__dirname, './config/index.js'), path.join(cli.helpers.configPath(), 'serviceoCore.js'))
+        await cli.copy(path.join(__dirname, './config', 'index.js'), path.join(cli.helpers.configPath(), 'serviceoCore.js'))
         cli.command.completed('create', 'config/serviceoCore.js')
     } catch (e) { }
 }
 
 module.exports = async function (cli) {
-    createConfigFile(cli)
+    await createConfigFile(cli)
 }
